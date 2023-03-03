7f79ee54df973b78bef5e1cd8c3dbd38aafa0f78
Joseph Rock <josephrockqz@gmail.com>
2023-03-02 22:19:57 -0500

    Implemented recycler view and card skeleton

--- app/src/main/java/com/example/artthiefdemo/ui/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/artthiefdemo/ui/overview/OverviewFragment.kt
@@ -4,44 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager.widget.ViewPager
 import com.example.artthiefdemo.R
-import com.example.artthiefdemo.databinding.FragmentOverviewBinding
-
-//class OverviewFragment : Fragment() {
-//
-//    private var _binding: FragmentOverviewBinding? = null
-//
-//    // This property is only valid between onCreateView and
-//    // onDestroyView.
-//    private val binding get() = _binding!!
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        val overviewViewModel =
-//            ViewModelProvider(this).get(OverviewViewModel::class.java)
-//
-//        _binding = FragmentOverviewBinding.inflate(inflater, container, false)
-//        val root: View = binding.root
-//
-//        val textView: TextView = binding.textOverview
-//        overviewViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
-//        return root
-//    }
-//
-//    override fun onDestroyView() {
-//        super.onDestroyView()
-//        _binding = null
-//    }
-//}
 
 class OverviewFragment : Fragment() {
 
--- app/src/main/java/com/example/artthiefdemo/ui/rate/RateFragment.kt
+++ app/src/main/java/com/example/artthiefdemo/ui/rate/RateFragment.kt
@@ -1,42 +1,30 @@
 package com.example.artthiefdemo.ui.rate
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.artthiefdemo.databinding.FragmentRateBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.artthiefdemo.R
 
 class RateFragment : Fragment() {
 
-    private var _binding: FragmentRateBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var rvList: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val rateViewModel =
-            ViewModelProvider(this).get(RateViewModel::class.java)
-
-        _binding = FragmentRateBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textRate
-        rateViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+    ): View? {
+        return inflater.inflate(R.layout.fragment_rate, container, false)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        rvList = view.findViewById(R.id.art_view)
+        rvList.layoutManager = LinearLayoutManager(activity)
+        rvList.adapter = RateArtAdapter()
     }
 }
