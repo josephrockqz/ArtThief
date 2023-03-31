eb0955504dd71f383a179e8b9a39d1734bae80ec
Joseph Rock <josephrockqz@gmail.com>
2023-03-31 15:37:06 -0400

    change menu icon colors

--- app/src/main/java/com/example/artthief/ui/rate/RateFragment.kt
+++ app/src/main/java/com/example/artthief/ui/rate/RateFragment.kt
@@ -2,6 +2,8 @@ package com.example.artthief.ui.rate
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Button
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -20,11 +22,13 @@ class RateFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        val searchView = getView()?.findViewById<SearchView>(R.id.mi_search)
+//        searchView?.color
 
         rvList = view.findViewById(R.id.art_view)
         rvList.layoutManager = LinearLayoutManager(activity)
         rvList.adapter = RateArtAdapter()
-
-        super.onViewCreated(view, savedInstanceState)
     }
 }
