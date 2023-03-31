7c0214ff040a8df5e867bc8728012c8e00296073
Joseph Rock <josephrockqz@gmail.com>
2023-03-31 12:01:55 -0400

    send fragment skeleton

--- app/src/main/java/com/example/artthief/ui/send/SendFragment.kt
+++ app/src/main/java/com/example/artthief/ui/send/SendFragment.kt
@@ -2,40 +2,16 @@ package com.example.artthief.ui.send
 
 import android.os.Bundle
 import android.view.*
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.artthief.databinding.FragmentSendBinding
+import com.example.artthief.R
 
 class SendFragment : Fragment() {
-
-    private var _binding: FragmentSendBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-        val rateViewModel =
-            ViewModelProvider(this).get(SendViewModel::class.java)
-
-        _binding = FragmentSendBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textSend
-        rateViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return inflater.inflate(R.layout.fragment_send, container, false)
     }
 }
