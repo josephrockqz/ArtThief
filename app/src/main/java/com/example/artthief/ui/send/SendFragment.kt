229939e36f1c819b2ab5f658be6a1da920b31844
Joseph Rock <josephrockqz@gmail.com>
2023-03-31 13:54:52 -0400

    send fragment styling

--- app/src/main/java/com/example/artthief/ui/send/SendFragment.kt
+++ app/src/main/java/com/example/artthief/ui/send/SendFragment.kt
@@ -2,6 +2,7 @@ package com.example.artthief.ui.send
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.artthief.R
 
@@ -13,5 +14,14 @@ class SendFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_send, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val numArtworks = 56
+        val buttonText = "Send $numArtworks Artworks"
+        val button = getView()?.findViewById<Button>(R.id.b_send_button)
+        button?.text = buttonText
     }
 }
