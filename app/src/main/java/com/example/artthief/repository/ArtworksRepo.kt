8b6be1ecd7df6f13a3772fbaffa810d6abb1f38b
Joseph Rock <josephrockqz@gmail.com>
2023-04-18 23:13:24 -0400

    Fixed frame skipping from rv

--- app/src/main/java/com/example/artthief/repository/ArtworksRepo.kt
+++ app/src/main/java/com/example/artthief/repository/ArtworksRepo.kt
@@ -1,6 +1,5 @@
 package com.example.artthief.repository
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
 import com.example.artthief.database.ArtworksDatabase
