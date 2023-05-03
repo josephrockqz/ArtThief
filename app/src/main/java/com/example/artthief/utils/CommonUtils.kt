893ba3b0941bf623c9e1b82215c77a561f8544fb
Joseph Rock <josephrockqz@gmail.com>
2023-05-03 19:52:42 -0400

    delete unused util function

--- app/src/main/java/com/example/artthief/utils/CommonUtils.kt
+++ app/src/main/java/com/example/artthief/utils/CommonUtils.kt
@@ -1,11 +1,3 @@
 package com.example.artthief.utils
 
-// TODO: get rid of this if not used
-fun stringifyArtworkDimensions(
-    width: Float,
-    height: Float
-): String {
-    val width = width.toString()
-    val height = height.toString()
-    return "$width\" by $height\""
-}
+// TODO: get rid of this file/package if not used
